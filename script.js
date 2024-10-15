249d71b840e2f44b8aef63cef3664b668850ae58
Benjamín <brj46@hi.is>
2024-10-15 17:17:55 +0000

    hreinsun og böggfixes

--- script.js
+++ script.js
@@ -31,23 +31,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 
-const scrollTopBtn = document.getElementById('scrollTopBtn');
 
 
-window.onscroll = function() { scrollFunction(); };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        scrollTopBtn.style.display = 'block';
-    } else {
-        scrollTopBtn.style.display = 'none';
-    }
-}
 
 
-scrollTopBtn.addEventListener('click', function() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
+
+
+
