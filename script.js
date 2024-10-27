33b14333bddefdb9c06126f4435b8f314517708f
Benjamín <brj46@hi.is>
2024-10-27 17:58:37 +0000

    on scroll gif playing

--- script.js
+++ script.js
@@ -30,6 +30,38 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    const projects = document.querySelectorAll('.project');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1
+    };
+
+    const observer = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(function (entry) {
+            const staticImg = entry.target.querySelector('.static-img');
+            const animatedImg = entry.target.querySelector('.animated-img');
+
+            if (entry.isIntersecting) {
+                if(staticImg)staticImg.style.display = 'none';
+                if(animatedImg)animatedImg.style.display = 'block';
+            }else{
+                if(staticImg)staticImg.style.display = 'block';
+                if(animatedImg)animatedImg.style.display = 'none';
+            }
+        });
+    }, observerOptions);
+
+    projects.forEach(function (project) {
+        observer.observe(project);
+    });
+});
+
+
+
+
 
 
 
